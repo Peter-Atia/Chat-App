58b06027fd42adafaead5be2a2c45e11c9d47b86
Peter-Atia <potrosatia1@gmail.com>
2025-07-07 15:43:30 +0300

    add .env

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:chat_app/views/log_in_view.dart';
 import 'package:chat_app/views/register_view.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
